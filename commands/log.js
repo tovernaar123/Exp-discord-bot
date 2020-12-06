d152da474db760756d5cb1555bd2ce71ccd856ba
PHIDIAS <phidiaslin0303@gmail.com>
2020-12-07 01:07:12 +0900

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -3,46 +3,48 @@ todo:
 put this logic in code
 reject starting line (sl) with the same logic as ending line
 only accept type empty, chat, or event.
+*/
 
-var msg = '';
-var type = 'chat';
-var sl = 0;
-
-msg = msg.split('\n');
-var msg_n = [];
-
-for (let i = 0; i < msg.length; i++) {
-    if (type == '') {
-        if (i > sl) {
-            msg_n.push(msg[i]);
-        }
-    }
-
-    else if (type == 'chat') {
-        if ((msg[i].indexOf('[CHAT]') != -1)) {
-            if (i > sl) {
-                msg_n.push(msg[i]);
+function log_chat(msg, type, sl) {
+    if (type == 'chat' || type == 'event' || type == '') {
+        msg = msg.split('\n');
+        var msg_n = [];
+        
+        for (let i = 0; i < msg.length; i++) {
+            if (type == '') {
+                if (i > sl) {
+                    msg_n.push(msg[i]);
+                }
+            }
+        
+            else if (type == 'chat') {
+                if ((msg[i].indexOf('[CHAT]') != -1)) {
+                    if (i > sl) {
+                        msg_n.push(msg[i]);
+                    }
+                }
+            }
+        
+            else if (type == 'event') {
+                if ((msg[i].indexOf('[CHAT]') == -1)) {
+                    if (i > sl) {
+                        msg_n.push(msg[i]);
+                    }
+                }
             }
         }
-    }
-
-    else if (type == 'event') {
-        if ((msg[i].indexOf('[CHAT]') == -1)) {
-            if (i > sl) {
-                msg_n.push(msg[i]);
-            }
-        }
+        
+        return msg_n.join('\n')
+    } else {
+        return 'Error - type unspecified'
     }
 }
 
-console.log(msg_n.join('\n'));
-
-*/
 
 const readLastLines = require(`read-last-lines`);
-async function get_logs(server, size, msg) {
+async function get_logs(server, size, msg, type, sl) {
     readLastLines.read(`/home/factorio/servers/eu-0${server}/console.log`, size) //D:\\programing\\lua\\factorio\\servers\\s${server}\\data\\console.log
-        .then((lines) => { let finalLines = lines.replace(/```/g, ',,,'); msg.channel.send(`\`\`\`${finalLines}\`\`\``, { split: true }) }) // lines gets returned with last X (see above) lines and sent to same channel as command
+        .then((lines) => { let finalLines = log_chat(lines.replace(/```/g, ',,,'), type, sl); msg.channel.send(`\`\`\`${finalLines}\`\`\``, { split: true }) }) // lines gets returned with last X (see above) lines and sent to same channel as command
         .then(console.log(`The last ${size} lines of the log for server #${server} posted in ${msg.channel.name}. (asked for ${size})`)) // logs the command in the console log
         .catch((err) => { internal_error(err); return });
 }
@@ -59,6 +61,22 @@ module.exports = {
     execute(msg, args, _, internal_error) {
         const server = Math.floor(Number(args[0]));
         let size = args[1];
+        
+        if (args.length > 2) {
+            if (Number.isInteger(args[2])) {
+                if (args[2] < args[1]) {
+                    let sl = args[2];
+                }                
+            } else {
+                let type = args[2];
+            }
+        }
+        
+        if (args.length > 3) {
+            let type = args[3];
+        }
+        
+        
         let sizeLimit = 50;
         let defaultSize = 10;
 
@@ -82,7 +100,7 @@ module.exports = {
         }
         if (server < 9 && server > 0) {
             console.log(`Server is ${server}`);
-            get_logs(server, size, msg)
+            get_logs(server, size, msg, type, sl)
                 .catch((err) => { internal_error(err); return })
         } else {
             msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp log <server#> <#lines>\``)
