f84c82968a5b26ddeb686e3a296640871d976125
tovernaar123 <56041037+tovernaar123@users.noreply.github.com>
2020-12-21 11:34:49 +0100

    Update commands/log.js
    Co-authored-by: ALo <39660675+aldldl@users.noreply.github.com>

--- commands/log.js
+++ commands/log.js
@@ -44,7 +44,7 @@ module.exports = {
             get_logs(server, size, msg)
                 .catch((err) => { internal_error(err); return })
         } else {
-            msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp unjail <server#> <username>\``)
+            msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp log <server#> <#oflines> <optionalStartingLine#>\``)
                 .catch((err) => { internal_error(err); return })
             console.log(`log look up by ${msg.author.username} incorrect server number`);
             return;
