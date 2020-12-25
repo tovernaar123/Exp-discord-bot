1c265a60e5811ad805bf135553e6275a130b5bad
PHIDIAS <phidiaslin0303@gmail.com>
2020-12-26 02:09:44 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
     cooldown: 5,
     async execute(msg, args) {
         const data = [];
-        const { commands } = msg.client;
+        const {commands} = msg.client;
         if (!args.length && msg.channel.type == "text") {
             let canKick = msg.member.hasPermission('KICK_MEMBERS');
             let isStaff = msg.member.roles.cache.has(role.staff);
