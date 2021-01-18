507130f2a5864af6898c7a95adc84ed2be40ae14
ALo <39660675+aldldl@users.noreply.github.com>
2021-01-18 15:30:23 -0500

    Update update.js
    msg not message

--- commands/update.js
+++ commands/update.js
@@ -34,8 +34,8 @@ module.exports = {
     usage: `<branch>`,
     execute(msg, args, _, internal_error) {
         
-        if(message.member.roles.cache.find(r => r.id === "678245941639381010") || message.member.roles.cache.find(r => r.id === "290940523844468738") message.member.roles.cache.find(r => r.id === "764526097768644618")) 
-        {// if Contributing Dev (PROD), Admin (PROD), Admin (test server)
+        if(msg.member.roles.cache.find(r => r.id === "678245941639381010") || msg.member.roles.cache.find(r => r.id === "290940523844468738") msg.member.roles.cache.find(r => r.id === "764526097768644618")) 
+        // if Contributing Dev (PROD), Admin (PROD), Admin (test server)
             run_command(msg, args, internal_error)
                 .catch((err) => { internal_error(err); return })   
         }else{msg.channel.send(`You do not seem to have the right role`); console.log('someone (${msg.member.displayName}) Tried to update the bot but does not have access')}
