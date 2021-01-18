74d837d192bb6b00c53966e8bcfbdf0e530f1440
ALo <39660675+aldldl@users.noreply.github.com>
2021-01-18 13:19:53 -0500

    Update pause.js
    Created Pause command

--- commands/pause.js
+++ commands/pause.js
@@ -1,1 +1,61 @@
 
+const Discord = require('discord.js');
+/**
+ * 
+ * @param {Number} server 
+ * @param {Rcon} rcon 
+ * @param {Discord.Message} msg 
+*/
+async function runcommand(server, rcon, msg) {
+    if (!rcon.connected) {
+        await msg.channel.send(`S${server} is not connected the bot.`)
+        return;
+    }
+    let res = await rcon.send(`/c game.tick_paused = true`) // Send command to clear pollution
+    if (!res) { // this command should not get a reply from the server. The command should print on the ingame server though.
+        rcon.send(`The server IS PAUSED BY A REMOTE ADMIN. - ${msg.member.displayName}. Please @staff on the discord if this was done by mistake.`);
+        await msg.channel.send(`No Error - Thus the game should have been **paused** on S${server}. Command Requested by *${msg.member.displayName}*.\n to unpause run \`${prefix} unpause <#>\``);
+        console.log(`${msg.author.displayName} has paused S${server}`);
+    } else {
+        await msg.channel.send(`Command might have failed result: \`\`\` ${res} \`\`\``);
+    } 
+}
+ 
+module.exports = {
+    name: 'pause',
+    aka: ['paused','stop'],
+    description: 'Clears Pollution (Currently Admin/Mod only command)',
+    guildOnly: true,
+    args: true,
+    helpLevel: 'staff',
+    required_role: role.staff,
+    usage: `\`<#>\` (Server Number, number only)`,
+    execute(msg, args, rcons, internal_error) {
+        let prefix = process.env.PREFIX;
+        const server = Math.floor(Number(args[0]));
+        //let reason = args.slice(2).join(" ");
+        let extra = args[1];
+        if (!server) {
+            msg.channel.send('Please pick a server first just a number (1-8)')
+                .catch((err) => { internal_error(err); return });
+            return;
+        }
+        if (extra) {
+            msg.channel.send(`No additional arguments needed. Correct usage: \`${prefix} pause <#>\` (Server Number only)\n **Command Not Run** `)
+                .catch((err) => { internal_error(err); return })
+            return;
+        }
+        if (server < 9 && server > 0) {
+            console.log(`Server is ${server}`);
+            console.log(`Server PAUSED cleared`)
+            runcommand(server, rcons[server], msg)
+                .catch((err) => { internal_error(err); return })
+        } else {
+            msg.reply(`Please pick a server first just a number (Currently 1-8). Correct usage is \`${prefix} pause <server#>\``)
+                .catch((err) => { internal_error(err); return })
+            console.log(`Server Pause ${msg.author.username} incorrect server number`);
+            return;
+        }
+    },
+};
+
