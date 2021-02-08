52e6e34c281b3b61f645f689a5eda7fbeb63a888
PHIDIAS <phidiaslin0303@gmail.com>
2021-02-08 17:14:36 +0900

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -9,14 +9,14 @@ shell.stderr.on('data', data => {
     console.error(`stderr: ${data}`);
 });
 
+
 async function run_command(msg, args, internal_error) {
     let branch = args[0]
-    
     if (branch) {
         shell.stdin.write(`git fetch; git checkout origin/${branch}\n`);
         await msg.reply('Updating bot will restart now')
         shell.stdin.write(`pm2 restart infoBot\n`);
-    } else {
+    }else{
         await msg.reply('need a branch.')
     }
 }
@@ -30,8 +30,14 @@ module.exports = {
     helpLevel: 'staff',
     required_role: role.staff,
     usage: `<branch>`,
-    async execute(msg, args, _, internal_error) {
-        run_command(msg, args, internal_error)
-            .catch((err) => {internal_error(err); return})
+    execute(msg, args, _, internal_error) {
+        
+    if (msg.member.roles.cache.find(r => r.id === "678245941639381010") || msg.member.roles.cache.find(r => r.id === "290940523844468738") || msg.member.roles.cache.find(r => r.id === "764526097768644618")) // if Contributing Dev (PROD), Admin (PROD), Admin (test server)
+        {
+            run_command(msg, args, internal_error)
+                .catch((err) => { internal_error(err); return })   
+        }
+    else {msg.channel.send(`You do not seem to have the right role`); console.log('someone (${msg.member.displayName}) Tried to update the bot but does not have access')}
+        
     }
 }
