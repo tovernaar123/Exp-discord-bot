d04d6e4dffe39eca9d8a2c7eaff5f8dc5a69c049
ALo <39660675+aldldl@users.noreply.github.com>
2021-02-09 13:09:28 -0500

    1

--- commands/playerdata.js
+++ commands/playerdata.js
@@ -26,7 +26,8 @@ module.exports = {
               let rawdata = fs.readFileSync('/home/exp_admin/api_v2/persistent_storage.json');
               let mydata = JSON.parse(rawdata);
               key1 = `${name}`; // in dataFile
-              console.log(mydata[rawdata][PlayerData][key1][Statistics]);
+              //console.log(mydata[rawdata][PlayerData][key1][Statistics]);
+              colsole.log(mydata);
         } else {
             msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp unjail <server#> <username>\``)
                 .catch((err) => { internal_error(err); return })
