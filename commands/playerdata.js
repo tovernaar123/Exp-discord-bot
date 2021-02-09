3a152aca63e376ca62ea8a9161ada77bb47f948e
ALo <39660675+aldldl@users.noreply.github.com>
2021-02-09 12:42:39 -0500

    Update playerdata.js
    $name = ${name}

--- commands/playerdata.js
+++ commands/playerdata.js
@@ -13,7 +13,7 @@ module.exports = {
     required_role: role.board,
     usage: ` <name>`,
     execute(msg, args, _, internal_error) {
-        const name = args[0];
+        let name = args[0];
         
         if (!name) {
             msg.channel.send('Please pick a name first. Just the name - CAPS COUNT')
@@ -25,7 +25,7 @@ module.exports = {
               const fs  = require('fs');
               let rawdata = fs.readFileSync('/home/exp_admin/api_v2/persistent_storage.json');
               let mydata = JSON.parse(rawdata);
-              key1 = {$name}; // in dataFile
+              key1 = ${name}; // in dataFile
               console.log(mydata[key1].settings);
         } else {
             msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp unjail <server#> <username>\``)
