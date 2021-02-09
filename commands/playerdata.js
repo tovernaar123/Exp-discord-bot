434e93e35e3a862ebfe31f963b9bd8741140bd9a
ALo <39660675+aldldl@users.noreply.github.com>
2021-02-09 12:36:43 -0500

    Test player data
    First test of player data

--- commands/playerdata.js
+++ commands/playerdata.js
@@ -1,2 +1,38 @@
 //player data storage access
 
+
+
+
+module.exports = {
+    name: 'playerdata',
+    aka: ['pd', 'userdata'],
+    description: 'get chat (last 10 lines) (Board+ command)',
+    guildOnly: true,
+    args: true,
+    helpLevel: 'staff',
+    required_role: role.board,
+    usage: ` <name>`,
+    execute(msg, args, _, internal_error) {
+        const name = args[0];
+        
+        if (!name) {
+            msg.channel.send('Please pick a name first. Just the name - CAPS COUNT')
+                .catch((err) => { internal_error(err); return });
+            return;
+        }
+          if (name) 
+          {
+              const fs  = require('fs');
+              let rawdata = fs.readFileSync('/home/exp_admin/api_v2/persistent_storage.json');
+              let mydata = JSON.parse(rawdata);
+              key1 = {$name}; // in dataFile
+              console.log(mydata[key1].settings);
+        } else {
+            msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp unjail <server#> <username>\``)
+                .catch((err) => { internal_error(err); return })
+            console.log(`log look up by ${msg.author.username} incorrect server number`);
+            return;
+        }
+
+    },
+};
