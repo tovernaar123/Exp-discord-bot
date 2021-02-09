7ad32fc0cb129a77194d1beaae1fd75c908e187e
ALo <39660675+aldldl@users.noreply.github.com>
2021-02-09 12:43:38 -0500

    Update playerdata.js
    `${name}` is not the same as ${name}

--- commands/playerdata.js
+++ commands/playerdata.js
@@ -25,7 +25,7 @@ module.exports = {
               const fs  = require('fs');
               let rawdata = fs.readFileSync('/home/exp_admin/api_v2/persistent_storage.json');
               let mydata = JSON.parse(rawdata);
-              key1 = ${name}; // in dataFile
+              key1 = `${name}`; // in dataFile
               console.log(mydata[key1].settings);
         } else {
             msg.reply(`Please pick a server first. Just the number (currently 1-8). Correct usage is \` .exp unjail <server#> <username>\``)
