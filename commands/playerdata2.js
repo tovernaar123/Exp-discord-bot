cb1e8400c12357b0bac26644fb9ee96aab74fc4b
Adam <git@aloimage.com>
2021-02-25 19:47:12 -0500

    Try with function

--- commands/playerdata2.js
+++ commands/playerdata2.js
@@ -11,165 +11,174 @@ module.exports = {
     usage: ` <name>`,
     execute(msg, args, _, internal_error) {
         const Discord = require('discord.js');
-        let name;
+        let name = args[0];
         name = msg.member.displayName;
-        name = args[0];
         
 
         
+        
+
+        if(name){playerdata2command(name,msg,args)}
+        if(!name){let name = msg.member.displayName; playerdata2command(name,msg,args)}
         
        // if (!name) {
        //     msg.channel.send('Please pick a name first. Just the name - CAPS COUNT')
        //         .catch((err) => { internal_error(err); return });
        //     return;
        // }
+        
+       function playerdata2command(name,msg,args)
+       {
         if (1 == 1) 
-        { // start PHIDAS format feature
+            { // start PHIDAS format feature
 
-            function ts(x) {
-                if (x === undefined) {
-                    return 0;
-                } else {
-                    try {
-                        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                    } catch(e) {
+                function ts(x) {
+                    if (x === undefined) {
                         return 0;
-                    }
-                }
-            }
-            
-            function profile(msg_2) {
-                var layout = ['Playtime', 'AfkTime', 'MapsPlayed', 'JoinCount', 'ChatMessages', 'CommandsUsed', 'RocketsLaunched', 'ResearchCompleted', 'MachinesBuilt', 'MachinesRemoved', 'TilesBuilt', 'TilesRemoved', 'TreesDestroyed', 'OreMined', 'ItemsCrafted', 'ItemsPickedUp', 'Kills', 'Deaths', 'DamageDealt', 'DistanceTravelled', 'CapsulesUsed', 'EntityRepaired', 'DeconstructionPlannerUsed', 'MapTagsMade'];
-                var layout_dict = {
-                    'Playtime':'Play Time',
-                    'AfkTime':'AFK Time',
-                    'MapsPlayed':'Maps Played',
-                    'JoinCount':'Join Count',
-                    'ChatMessages':'Chat Messages',
-                    'CommandsUsed':'Commands',
-                    'RocketsLaunched':'Rockets Launched',
-                    'ResearchCompleted':'Research Completed',
-                    'MachinesBuilt':'Machines Built',
-                    'MachinesRemoved':'Machines Removed',
-                    'TilesBuilt':'Tiles Placed',
-                    'TilesRemoved':'Tiles Removed',
-                    'TreesDestroyed':'Trees Destroyed',
-                    'OreMined':'Ore Mined',
-                    'ItemsCrafted':'Items Crafted',
-                    'ItemsPickedUp':'Items Picked Up',
-                    'Kills':'Kills',
-                    'Deaths':'Deaths',
-                    'DamageDealt':'Damage Dealt',
-                    'DistanceTravelled':'Distance Travelled',
-                    'CapsulesUsed':'Capsules Used',
-                    'EntityRepaired':'Machines Repaired',
-                    'DeconstructionPlannerUsed':'Decon Planner Used',
-                    'MapTagsMade':'Map Tags Created'
-                };
-                
-                var msg_4 = [];
-            
-                for (let i = 0; i < layout.length; i++) {
-                    var msg_3 = [];
-                    
-                    if (layout[i] == 'Playtime' || layout[i] == 'AfkTime') {
-                        try {
-                            var t1 = msg_2[layout[i]];
-                        } catch (e) {
-                            var t1 = 0;
-                        }
-                            
-                        var h1 = Math.floor(t1 / 60);
-                        var m1 = Math.floor(t1 % 60);
-            
-                        if (h1 > 0) {
-                            msg_3.push(layout_dict[layout[i]]);
-                            msg_3.push(h1 + ' h ' + m1 + ' m');
-                        } else {
-                            msg_3.push(layout_dict[layout[i]]);
-                            msg_3.push(m1 + ' m ');
-                        }
-            
                     } else {
                         try {
-                            msg_3.push(layout_dict[layout[i]]);
-                            msg_3.push(ts(msg_2[layout[i]]));
-                        } catch (e) {
-                            msg_3.push(layout_dict[layout[i]]);
-                            msg_3.push(ts(0));
+                            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                        } catch(e) {
+                            return 0;
                         }
                     }
+                }
+                
+                function profile(msg_2) {
+                    var layout = ['Playtime', 'AfkTime', 'MapsPlayed', 'JoinCount', 'ChatMessages', 'CommandsUsed', 'RocketsLaunched', 'ResearchCompleted', 'MachinesBuilt', 'MachinesRemoved', 'TilesBuilt', 'TilesRemoved', 'TreesDestroyed', 'OreMined', 'ItemsCrafted', 'ItemsPickedUp', 'Kills', 'Deaths', 'DamageDealt', 'DistanceTravelled', 'CapsulesUsed', 'EntityRepaired', 'DeconstructionPlannerUsed', 'MapTagsMade'];
+                    var layout_dict = {
+                        'Playtime':'Play Time',
+                        'AfkTime':'AFK Time',
+                        'MapsPlayed':'Maps Played',
+                        'JoinCount':'Join Count',
+                        'ChatMessages':'Chat Messages',
+                        'CommandsUsed':'Commands',
+                        'RocketsLaunched':'Rockets Launched',
+                        'ResearchCompleted':'Research Completed',
+                        'MachinesBuilt':'Machines Built',
+                        'MachinesRemoved':'Machines Removed',
+                        'TilesBuilt':'Tiles Placed',
+                        'TilesRemoved':'Tiles Removed',
+                        'TreesDestroyed':'Trees Destroyed',
+                        'OreMined':'Ore Mined',
+                        'ItemsCrafted':'Items Crafted',
+                        'ItemsPickedUp':'Items Picked Up',
+                        'Kills':'Kills',
+                        'Deaths':'Deaths',
+                        'DamageDealt':'Damage Dealt',
+                        'DistanceTravelled':'Distance Travelled',
+                        'CapsulesUsed':'Capsules Used',
+                        'EntityRepaired':'Machines Repaired',
+                        'DeconstructionPlannerUsed':'Decon Planner Used',
+                        'MapTagsMade':'Map Tags Created'
+                    };
+                    
+                    var msg_4 = [];
+                
+                    for (let i = 0; i < layout.length; i++) {
+                        var msg_3 = [];
+                        
+                        if (layout[i] == 'Playtime' || layout[i] == 'AfkTime') {
+                            try {
+                                var t1 = msg_2[layout[i]];
+                            } catch (e) {
+                                var t1 = 0;
+                            }
+                                
+                            var h1 = Math.floor(t1 / 60);
+                            var m1 = Math.floor(t1 % 60);
+                
+                            if (h1 > 0) {
+                                msg_3.push(layout_dict[layout[i]]);
+                                msg_3.push(h1 + ' h ' + m1 + ' m');
+                            } else {
+                                msg_3.push(layout_dict[layout[i]]);
+                                msg_3.push(m1 + ' m ');
+                            }
+                
+                        } else {
+                            try {
+                                msg_3.push(layout_dict[layout[i]]);
+                                msg_3.push(ts(msg_2[layout[i]]));
+                            } catch (e) {
+                                msg_3.push(layout_dict[layout[i]]);
+                                msg_3.push(ts(0));
+                            }
+                        }
+                
+                        msg_4.push([msg_3[0], msg_3[1]]);
+                    }
+                    
+                    return msg_4;
+                }
+                //find items
+                const fs  = require('fs');
+                let rawdata = fs.readFileSync('/home/exp_admin/api_v2/persistent_storage.json');
+                let mydata = JSON.parse(rawdata);
+                key1 = `${name}`; // in dataFile
+                key2 = `Statistics`;
+                let checkdata = mydata["PlayerData"][key1];
+                let finaldata;
+                if(!checkdata)
+                {msg.channel.send('Name error: Name not found in datastore');console.log(`Name Not Found`); return;}
+                
+                finaldata = mydata["PlayerData"][key1]["Statistics"];
+                item = finaldata;
+
+
+                let channel = msg.channel;
+                let author = msg.author.username;
+                let result = profile(item);
+                let lookup = key1;
+
+                channel.send(`\`\`\`Player Data\nRequested by ${msg.member.displayName}\nUsername: ${lookup}\n\`\`\``);
+
+                const Embed = new Discord.MessageEmbed();
+                Embed.setColor("0x00ff00");
+
+                for (let i = 0; i < result.length/2; i+=2) {
+                    try {
+                        Embed.addField(result[i][0], result[i][1], true);
+                    } catch (e) {
+                        Embed.addField(`** **`, `** **`, true);
+                    }
+
+                    try {
+                        Embed.addField(result[i+1][0], result[i+1][1], true);
+                    } catch (e) {
+                        Embed.addField(`** **`, `** **`, true);
+                    }
+
+                    Embed.addField(`** **`, `** **`, true);
+                }
+
+                const Embed2 = new Discord.MessageEmbed();
+                Embed2.setColor("0x00ff00");
+
+                for (let i = 0; i < result.length/2; i+=2) {
+                    let j = i + result.length/2;
+                    try {
+                        Embed2.addField(result[j][0], result[j][1], true);
+                    } catch (e) {
+                        Embed2.addField(`** **`, `** **`, true);
+                    }
+
+                    try {
+                        Embed2.addField(result[j+1][0], result[j+1][1], true);
+                    } catch (e) {
+                        Embed2.addField(`** **`, `** **`, true);
+                    }
+                    
+                    Embed2.addField(`** **`, `** **`, true);
+                }
+
+                channel.send(Embed);
+                channel.send(Embed2);
+                channel.send();
+                return;
             
-                    msg_4.push([msg_3[0], msg_3[1]]);
-                }
-                
-                return msg_4;
-            }
-            //find items
-            const fs  = require('fs');
-             let rawdata = fs.readFileSync('/home/exp_admin/api_v2/persistent_storage.json');
-             let mydata = JSON.parse(rawdata);
-              key1 = `${name}`; // in dataFile
-              key2 = `Statistics`;
-              let checkdata = mydata["PlayerData"][key1];
-              let finaldata;
-              if(!checkdata)
-              {msg.channel.send('Name error: Name not found in datastore');console.log(`Name Not Found`); return;}
-              
-              finaldata = mydata["PlayerData"][key1]["Statistics"];
-              item = finaldata;
 
-
-            let channel = msg.channel;
-            let author = msg.author.username;
-            let result = profile(item);
-            let lookup = key1;
-
-            channel.send(`\`\`\`Player Data\nRequested by ${msg.member.displayName}\nUsername: ${lookup}\n\`\`\``);
-
-            const Embed = new Discord.MessageEmbed();
-            Embed.setColor("0x00ff00");
-
-            for (let i = 0; i < result.length/2; i+=2) {
-                try {
-                    Embed.addField(result[i][0], result[i][1], true);
-                } catch (e) {
-                    Embed.addField(`** **`, `** **`, true);
-                }
-
-                try {
-                    Embed.addField(result[i+1][0], result[i+1][1], true);
-                } catch (e) {
-                    Embed.addField(`** **`, `** **`, true);
-                }
-
-                Embed.addField(`** **`, `** **`, true);
-            }
-
-            const Embed2 = new Discord.MessageEmbed();
-            Embed2.setColor("0x00ff00");
-
-            for (let i = 0; i < result.length/2; i+=2) {
-                let j = i + result.length/2;
-                try {
-                    Embed2.addField(result[j][0], result[j][1], true);
-                } catch (e) {
-                    Embed2.addField(`** **`, `** **`, true);
-                }
-
-                try {
-                    Embed2.addField(result[j+1][0], result[j+1][1], true);
-                } catch (e) {
-                    Embed2.addField(`** **`, `** **`, true);
-                }
-                
-                Embed2.addField(`** **`, `** **`, true);
-            }
-
-            channel.send(Embed);
-            channel.send(Embed2);
-            channel.send();
-            return;
+        
 
 
 
@@ -268,6 +277,6 @@ module.exports = {
             console.log(`log look up by ${msg.author.username} incorrect server number`);
             return;
         }
-
+       }
     },
 };
