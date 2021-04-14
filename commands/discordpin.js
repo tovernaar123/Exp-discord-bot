b4c4e390b9ea9ff7ca5f4b0b07570a24d09c13ff
Adam <git@aloimage.com>
2021-04-14 14:32:50 -0400

    msg.pin
    maybe msg.pin will work

--- commands/discordpin.js
+++ commands/discordpin.js
@@ -29,7 +29,7 @@ module.exports = {
 
 
         msg.channel.messages.fetch(idToPin)
-        .then(message => message.pin())
+        .then(msg => msg.pin())
         .catch((err) => { internal_error(err); return })
     },
 };
