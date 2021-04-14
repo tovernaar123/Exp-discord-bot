6bd1765c693ecb3a537d405f8df8f6a8ce5543f5
Adam <git@aloimage.com>
2021-04-14 14:48:26 -0400

    Discord Pin clean up

--- commands/discordpin.js
+++ commands/discordpin.js
@@ -2,37 +2,20 @@ const { Role } = require("discord.js");
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'pin',
+    name: 'discordpin',
     aka: ['pin', 'pinpost'],
-    description: 'pin a post (Mod+ only)',
+    description: 'Pins a post in our discord. Must run command in the same channel as the message you want to pin (Mod+ only)',
     guildOnly: true,
     args: true,
     helpLevel: 'role.staff',
     required_role: role.staff,
     usage: `<id>`,
     execute(msg, args, _, internal_error) {
-        //const idToPin = Math.floor(Number(args[0]));
-        let idToPin = args[0];
-        /*
-        if (!idToPin) {
-            msg.channel.send('please pick an id first');
-            console.log(`wanted to pin but did not pick an id first`);
-            return;
-        }
 
-        if (isNaN(idToPin)) { // Checks if the `amount` parameter is a number. If not, the command throws an error
-            msg.reply('The id is not a number');
-            console.log(`id is not an id/number`);
-            return;
-        }
-        if (idToPin < 1) {
-            msg.reply('an id number can not be this small'); // makes sure 1 or more posts  
-            return;
-        }
-        */
+        let idToPin = args[0];
 
         msg.channel.messages.fetch(idToPin)
-        .then(msg => msg.pin())
-        .catch((err) => { internal_error(err); return })
+            .then(msg => msg.pin())
+            .catch((err) => { internal_error(err); return })
     },
 };
