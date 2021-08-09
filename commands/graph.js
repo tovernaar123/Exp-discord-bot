b2dddd5da4147e5faeab64d2e9117271b98f1c35
PHIDIAS <phidiaslin0303@gmail.com>
2021-08-09 19:10:28 +0900

    Update graph.js

--- commands/graph.js
+++ commands/graph.js
@@ -1,47 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const request = require('request');
-
-function graph_type_select(type) {
-    switch (type) {
-        case 'load':
-            type = 4;
-            break;
-        case 'memory':
-            type = 7;
-            break;
-        case 'network':
-            type = 9;
-            break;
-        case 'disk':
-            type = 10;
-            break;
-        case 'usage':
-            type = 11;
-            break;
-        case 'cpu_temp':
-            type = 28;
-            break;
-        case 'cpu_freq':
-            type = 42;
-            break;
-        case 'cpu':
-            type = 51;
-            break;
-        case 'player':
-            type = 53;
-            break;
-        case 'ups':
-            type = 54;
-            break;
-        case 'rocket':
-            type = 55;
-            break;
-        default:
-            type = 54;
-    }
-    return type;
-}
+const puppeteer = require('puppeteer');
 
 module.exports = {
     name: 'graph',
@@ -55,61 +14,85 @@ module.exports = {
         // let server = args[0].replace(/server|s/i, '');
         // server = Number(server) || server;
         let channel = msg.channel;
-        let msg = [];
         let type = args[0].toLowerCase();
 
         //board
         let role_needed = role.board;
         let role = await msg.guild.roles.fetch(role_needed);
         let allowedThisCommand = msg.member.roles.highest.comparePositionTo(role) >= 0;
-        let graph_index_list = [4, 7, 9, 10, 11, 28, 42, 51, 53, 54, 55];
-        let graph_index_allowed = [53, 54, 55];
 
-        if (type) {
-            if (isNaN(+type)) {
-                // Type is word
-                type = graph_type_select(type)
+        let graph_list_standard = ['54', '55', '56'];
+        let graph_list = ['4', '7', '9', '10', '11', '28', '42', '51', '54', '55', '56'];
+        let graph_dict = {
+            'load':'4',
+            'memory':'7',
+            'network':'9',
+            'disk':'10',
+            'usage':'11',
+            'cpu_temp':'28',
+            'cpu_freq':'42',
+            'cpu':'51',
+            'player':'54',
+            'ups':'55',
+            'rocket':'56'
+        };
+
+        
+        if (isNaN(type)) {
+            // Type is word
+            if (graph_dict.hasOwnProperty(type)) {
+                type = graph_dict(type);
             } else {
-                // Type is number
-                if (graph_index_list.indexOf(type) < 0) {
-                    channel.send(`Error: Lookup out of range.`);
-                    type = -1;
-                }
-            }
-            
-            if (!allowedThisCommand) {
-                if (graph_index_allowed.indexOf(type) < 0) {
-                    channel.send(`Error: Unauthorized use of advanced graph usage.`);
-                    type = -1;
-                }
+                channel.send({content: `Error: Lookup out of range.`});
             }
         } else {
-            type = 54;
+            // Type is number
+            if (graph_list.indexOf(type) < 0) {
+                channel.send({content: `Error: Lookup out of range.`});
+                type = -1;
+            }
+        }
+            
+        if (!allowedThisCommand) {
+            if (graph_list_standard.indexOf(type) < 0) {
+                channel.send({content: `Error: Unauthorized use of other graph usage.`});
+                type = -1;
+            }
         }
 
         if (type >= 0) {
-            const download = (url, path, callback) => {
-                request.head(url, (err, res, body) => {
-                  request(url).pipe(fs.createWriteStream(path)).on('close', callback);
-                })
-            }
+            async function download(url, path, width, height) {
+                const browser = await puppeteer.launch({
+                    headless: false
+                });
+                const page = await browser.newPage();
+                await page.setViewport({width: width, height: height});
+                await page.goto(url);
+                
+                fs.writeFile(path, await viewSource.buffer(), function (err) {
 
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            
+                browser.close();
+            }
+            
             let url = 'https://info.explosivegaming.nl/grafana/render/d-solo/wRgzuFqiz/system-metrics?orgId=1&from=now-30m&to=now&panelId=' + type + '&width=1000&height=300&tz=UTC';
-            const path = '.cache/graph.png'
+            const path = './graph.png'
     
             try {
-                download(url, path, () => {
-                    console.log('Download Complete.');
-                })
+                download(url, path, 1000, 300);
             } catch (e) {
-                channel.send(`Error when saving image.`);
-                console.log(`Error when saving graph image.`);
+                channel.send({content: `Error when saving image.`});
+                console.log({content: `Error when saving graph image.`});
             }
     
             try {
-                channel.send({files: ['.cache/graph.png']});
+                channel.send({files: [path]});
             } catch (e) {
-                channel.send(`Error when sending image.`);
+                channel.send({content: `Error when sending image.`});
                 console.log(`Error when sending image.`);
             }
         }
