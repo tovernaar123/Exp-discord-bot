6808d926f3ff39d2adfb5ec8eb6a4e4a06c70597
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2021-08-11 02:56:42 +0900

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'afk',
     aka: ['whoisafk', 'afkstreak', 'alwaysafk'],
-    description: 'how many players are online?',
+    description: 'Get numbers of players afk (Mod+)',
     guildOnly: true,
     args: true,
     required_role: role.staff,
