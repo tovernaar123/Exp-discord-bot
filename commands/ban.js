8a284ab87cbd2e9f8a038150ec4f7368c5228c8a
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2021-08-11 02:46:25 +0900

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,26 +1,28 @@
 const Discord = require('discord.js');
 
-async function runCommand(server, rcon, msg, toBan, reason) {
+async function runCommand(server, rcon, msg, username, reason) {
     if(!rcon.connected){
         await msg.channel.send(`S${server} is not connected the bot.`)
         return;
     }
 
-    await rcon.send(`/ban ${toBan} ${reason}`);
-    await msg.channel.send(`User **${toBan}** has been Banned for *${reason}*, check with someone on S${server} to be sure `);
+    await rcon.send(`/ban ${username} ${reason}`);
+    await msg.channel.send(`User **${username}** has been Banned for *${reason}*, check with someone on S${server} to be sure `);
+    
     const Embed = Discord.MessageEmbed();
     Embed.addField('Ban', `A player has been Banned`, false);
     Embed.addField(`Server Details`, `server: eu-${server}`, false);
-    Embed.addField(`Player`, `${toBan}`, true);
+    Embed.addField(`Player`, `${username}`, true);
     Embed.addField(`By`, `${msg.author.displayName}`, true);
     Embed.addField(`Reason`, `${reason}`, true);
     Embed.setColor("0xb40e0e");
+
     // Reports channel is "368812365594230788" for exp 
     // Reports Channel is "764881627893334047" for test server
     let report_channel = msg.guild.channels.cache.get('368812365594230788');
+
     await report_channel.send({embeds: [Embed]});
 }
-
 
 module.exports = {
     name: 'ban',
@@ -48,10 +50,10 @@ module.exports = {
             return;
         }
 
-        let toBan = args[1];
+        let username = args[1];
         let reason = args.slice(2).join(" ");
         
-        if (!toBan) { 
+        if (!username) { 
             msg.channel.send({content: `Username is required to ban. Correct usage: \`.exp ban<#> <username> <reason>\``});
             console.log(`Error: Command - Ban did not have username supplied.`);
             return;
@@ -65,13 +67,7 @@ module.exports = {
 
         if (server < 9 && server > 0) {
             console.log(`Info: Command - Ban server is ${server}.`);
-            runCommand(server, rcons[server], msg, toBan, reason)
-                .catch((err) => { internal_error(err); return })
-        } else {
-            // If a person DID give a server number but did NOT give the correct one it will return without running - is the server number is part of the array of the servers it could be (1-8 currently)
-            msg.reply(`Please pick a server first just a number (1-8) or *all*.  Correct usage is \`.exp ban <server#>\``)
-                .catch((err) => { internal_error(err); return })
-            console.log(`players online by ${author} incorrect server number`);
+            runCommand(server, rcons[server], msg, username, reason).catch((err) => {internal_error(err); return});
         }
     },
 };
