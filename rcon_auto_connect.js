e7303788d198217dafbfd77424b1b090015ae088
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2021-08-11 20:03:46 +0900

    Update rcon_auto_connect.js

--- rcon_auto_connect.js
+++ rcon_auto_connect.js
@@ -1,14 +1,14 @@
 "use strict";
 const rcon_client = require("rcon-client");
 const rconpw = process.env.RCONPASS;
+
 //Set up RCON connections for all servers.
 /**
     @param {number} port The port to connect the rcon too.
     @param {number} port The id of the server the server will be called S + id.
     @returns {object} Object that contains send (to send commands) and connected (to check the connection).
 */
-exports.rcon_connect = async function(port, i) {
-    
+exports.rcon_connect = async function(port, i) { 
     const client = new rcon_client.Rcon({
         host: "localhost",
         port: port,
@@ -18,12 +18,14 @@ exports.rcon_connect = async function(port, i) {
     let Iconnected = false;
     //Show commands sent and their reply for debugging
     let real_send = client.send;
+
     client.send = function(cmd) {
         return real_send.call(client, cmd).then(res => {
             if(!(client.socket && client.socket.writable && client.authenticated && Iconnected)){
                 console.error(`[Rcon]: Not connected tried to send: ${cmd}`);
                 return;
             }
+
             console.log(`S${i}`, cmd, "=>", res.slice(0, -1));
             return res;
         });
@@ -59,13 +61,15 @@ exports.rcon_connect = async function(port, i) {
     });
 
     client.on("error", function(err) {
-        console.log(`Error on rcon for S${i}`, err.message);
+        console.log(`Error: Rcon for S${i} `, err.message);
     });
+
     await connect();
+
     return {
         send: client.send,
         get connected(){
-            return client.socket && client.socket.writable && client.authenticated && Iconnected
+            return client.socket && client.socket.writable && client.authenticated && Iconnected;
         }
     }
 };
