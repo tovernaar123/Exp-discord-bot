b6e4e8da5295f0231199895b8fe6199d1287d01b
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2021-09-22 15:59:33 +0900

    Update playerdatajson.js

--- commands/playerdatajson.js
+++ commands/playerdatajson.js
@@ -1,4 +1,5 @@
 //player data storage access
+const fs = require('fs');
 
 function playerdata1command(name, msg) {
     // Find "items" from datastore 
@@ -14,16 +15,22 @@ function playerdata1command(name, msg) {
     // Checks to see if any data was retured at all, if the name is not in the database, or the database is not accessable than it will return an error and stop running the command
     
     if (!checkdata) {
-        msg.channel.send({content: 'Error: Name not found. Check the name or try again later.'});
-        console.log(`Name not found`);
+        msg.channel.send('Error: Name not found. Check the name or try again later.');
+        console.log(`Name '${key1}' not found`);
         return;
     }
-
+    let privacyData = mydata["PlayerData"][key1]["DataSavingPreference"];
+    if (privacyData) {
+        msg.channel.send('Error: Privacy Settings Prevent Lookup. Check the name or try again later after turning on Data sync.');
+        console.log(`Privacy settings for user ${key1} prevent saved stats`);
+        return;
+    }
     // if it didnt stop based on the name not returining it will then filter out only the Statistics (removing prefrences like alt mode, join msg etc)
     let finaldata = mydata["PlayerData"][key1]["Statistics"];
 
     let channel = msg.channel;
     channel.send(`${name}:\n\`\`\`json\n${JSON.stringify(finaldata, null, 2)}\`\`\``);
+    console.log(`${name} json player data printed to ${channel}`);
     return;
 }
 
@@ -32,27 +39,32 @@ module.exports = {
     aka: ['pd1', 'pdj', 'userdatajson', 'oguserdata','pdjson','pdog'],
     description: 'Get stats (datastore info) for any user (Board+) (No formatting, Json output)',
     guildOnly: true,
-    args: true,
-    helpLevel: 'board',
-    required_role: role.board,
-    usage: ` <nameToLookup>`,
+    args: false, //can have args not needed
+    helpLevel: 'all', // helpLevel places it in the "semi-public" group
+    //required_role: role.board,
+    usage: ` <nameToLookup> || <null> (for self lookup)`,
     async execute(msg, args, _, internal_error) {
+        
+        async function runCommand() {
         //board
         let role_needed = await msg.guild.roles.fetch(role.board);
         let allowedThisCommand = msg.member.roles.highest.comparePositionTo(role_needed) >= 0; 
         let name = args[0];
 
         if (name) {
-            if (allowedThisCommand) {
+            if (allowedThisCommand || name === msg.member.displayName) {
                 // If the user is authorized to use the command and supplied a name
                 playerdata1command(name, msg);
             } else {
-                msg.channel.send({content: `Error: You are not authorized to perform this action.`});
+                msg.channel.send(`Error: You are not authorized to perform this action.`);
+                console.log(`user ${msg.member.displayName} tried to look up ${name}'s user data - it was not allowed due to lack of permissions`);
             }
         } else {
             // User doesnt need to get authorized for a self lookup
             name = msg.member.displayName;
             playerdata1command(name, msg);
+            }
         }
+        runCommand();
     },
 };
